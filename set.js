c76655973e4d9d38935c498f3ad836b71a93c418
Jim de Beer <jim@vigour.io>
2016-12-19 13:20:24 +0100

    clean up, set tests

--- set.js
+++ set.js
@@ -6,42 +6,58 @@ const State = require('vigour-state') // eslint-disable-line
 console.log('benchmark context: n = ' + ((n / 1e3) | 0) + 'k')
 
 perf(() => {
+  var cnt = 0
   const orig = create({
     types: {
       ha: { bla: 'hello' }
     },
     a: { type: 'ha' }
   })
-  orig.subscribe({ a: { bla: true } }, () => {})
+  orig.subscribe({
+    a: { bla: true }
+  }, () => { cnt++ })
   for (let i = 0; i < n; i++) {
     get(orig.types.ha, 'bla').set(i)
   }
 }, () => {
+  var cnt = 0
   const orig = create({
-    a: { bla: 'hello' }
+    a: { bla: 'hello'  }
   })
-  orig.subscribe({ a: { bla: true } }, () => {})
+  orig.subscribe({
+    a: { bla: true }
+  }, () => {
+    cnt++
+  })
   for (let i = 0; i < n; i++) {
     get(orig.a, 'bla').set(i)
   }
 }, 'simple subscription')
 
 perf(() => {
+  var cnt = 0
   const orig = new State({
     types: {
       ha: { bla: 'hello' }
     },
     a: { type: 'ha' }
   })
-  orig.subscribe({ a: { bla: { val: true } } }, () => {})
+  orig.subscribe({
+    a: { bla: { val: true } }
+  }, () => { cnt++ })
   for (let i = 0; i < n; i++) {
     orig.types.ha.bla.set(i)
   }
 }, () => {
+  var cnt = 0
   const orig = new State({
-    a: { bla: 'hello' }
+    a: { bla: 'hello'  }
   })
-  orig.subscribe({ a: { bla: { val: true } } }, () => {})
+  orig.subscribe({
+    a: { bla: { val: true } }
+  }, () => {
+    cnt++
+  })
   for (let i = 0; i < n; i++) {
     orig.a.bla.set(i)
   }
