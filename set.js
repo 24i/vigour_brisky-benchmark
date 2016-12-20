cb35ee8112db49b8ab1640dc8cc32c681f627a63
Jim de Beer <jim@vigour.io>
2016-12-20 13:47:52 +0100

    deep object

--- set.js
+++ set.js
@@ -1,64 +1,29 @@
 const perf = require('brisky-performance')
-const { create, get } = require('brisky-struct')
-const n = 1e5
-const State = require('vigour-state') // eslint-disable-line
+const { create, get, set } = require('brisky-struct')
+const n = 1e6
 
 console.log('benchmark context: n = ' + ((n / 1e3) | 0) + 'k')
 
-perf(() => {
-  var cnt = 0
-  const orig = create({
-    types: {
-      ha: { bla: 'hello' }
-    },
-    a: { type: 'ha' }
-  })
-  orig.subscribe({
-    a: { bla: true }
-  }, () => { cnt++ })
-  for (let i = 0; i < n; i++) {
-    get(orig.types.ha, 'bla').set(i)
+const simple = () => {
+  const s = create()
+  let i = n
+  while (i--) {
+    set(s, i)
   }
-}, () => {
-  var cnt = 0
-  const orig = create({
-    a: { bla: 'hello'  }
-  })
-  orig.subscribe({
-    a: { bla: true }
-  }, () => {
-    cnt++
-  })
-  for (let i = 0; i < n; i++) {
-    get(orig.a, 'bla').set(i)
+}
+
+perf(simple, () => {
+  const s = {}
+  let i = n
+  while (i--) {
+    s.val = i
   }
-}, 'simple subscription')
+}, 'simple set', 25)
 
 perf(() => {
-  var cnt = 0
-  const orig = new State({
-    types: {
-      ha: { bla: 'hello' }
-    },
-    a: { type: 'ha' }
-  })
-  orig.subscribe({
-    a: { bla: { val: true } }
-  }, () => { cnt++ })
-  for (let i = 0; i < n; i++) {
-    orig.types.ha.bla.set(i)
+  const s = create()
+  let i = n
+  while (i--) {
+    set(s, { val: i })
   }
-}, () => {
-  var cnt = 0
-  const orig = new State({
-    a: { bla: 'hello'  }
-  })
-  orig.subscribe({
-    a: { bla: { val: true } }
-  }, () => {
-    cnt++
-  })
-  for (let i = 0; i < n; i++) {
-    orig.a.bla.set(i)
-  }
-}, 'simple subscription (old)')
+}, simple, 'simple set', 2)
