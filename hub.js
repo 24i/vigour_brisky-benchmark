0cd571e993abe22656d4cff54757541760750ca7
Jim de Beer <jim@vigour.io>
2016-12-29 11:13:16 +0100

    added slow test

--- hub.js
+++ hub.js
@@ -1,12 +1,31 @@
-const { create, get, set } = require('brisky-struct')
+const hub = require('hub.js')
 const n = 1e3
 
 console.log('benchmark hub.js: n = ' + ((n / 1e3) | 0) + 'k')
 
-const simple = () => {
-  const s = create()
-  let i = n
-  while (i--) {
-    set(s, i)
+const server = hub()
+
+const someData = {}
+
+const val = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ' +
+  'incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ' +
+  'exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure ' +
+  'dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+
+let i = 1e3
+while (i-- > 0) {
+  const d = 1e11 + Math.round(Math.random() * 1e11)
+  someData[`key-${d}-longer-string-${d}`] = {
+    keyOne: { subKeyOne: val, subKeyTwo: val },
+    keyTwo: { subKeyOne: val, subKeyTwo: val },
+    keyThree: { subKeyOne: val, subKeyTwo: val },
+    keyFour: { subKeyOne: val, subKeyTwo: val },
+    keyFive: { subKeyOne: val, subKeyTwo: val }
   }
 }
+
+server.set({ someData })
+
+var d = Date.now()
+server.set(null)
+console.log(Date.now() - d, 'ms')
